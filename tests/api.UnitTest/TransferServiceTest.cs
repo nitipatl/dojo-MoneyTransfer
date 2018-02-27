c2ad4f02d80fcd0500c8654adde5dfce68fda22d
iFew <chitpong@gmail.com>
2018-02-27 14:26:35 +0700

    Changed refactoring expected data

--- tests/api.UnitTest/TransferServiceTest.cs
+++ tests/api.UnitTest/TransferServiceTest.cs
@@ -7,13 +7,11 @@ namespace api.UnitTest
     public class TransferServiceTest
     {
         [Theory]
-        [InlineData(0.00)]
-        [InlineData(500.00)]
-        [InlineData(100000.00)]
-        public void When_Accumulated_Between_0_And_100000(double accumulatedNumber)
+        [InlineData(0.00, true)]
+        [InlineData(500.00, true)]
+        [InlineData(100000.00, false)]
+        public void When_Accumulated_Between_0_And_100000(double accumulatedNumber, bool expectedResult)
         {
-            bool expectedResult = true;
-
             TransferService transferService = new TransferService();
             bool actualResult = transferService.IsAccumulated(accumulatedNumber);
 
