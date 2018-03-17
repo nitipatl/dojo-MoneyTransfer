1c62016a57db4a55a3daa47183bc0f694db9d542
iFew <chitpong@gmail.com>
2018-03-18 00:05:53 +0700

    Change for split condition support unit test and call service to support integration test

---
+++ src/api/Services/TransferCondition.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using api.Models;
+
+namespace api.Services
+{
+    public class TransferCondition
+    {
+        public bool IsAccumulated(double accumulatedNumber)
+        {
+            if((accumulatedNumber >= 0) && (accumulatedNumber < 100000)) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsTransferAmount(double transferAmount)
+        {
+            if((transferAmount >= 1) && (transferAmount <= 20000)) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsTransferFee(double transferFee)
+        {
+            if(transferFee > 0) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsTotalTransferNotOverAccumulated(double transferAmountAndFee, double accumulatedNumber)
+        {
+            double transferTotal = transferAmountAndFee + accumulatedNumber;
+            if((transferTotal > 0) && (transferTotal <= 100000)) {
+                return true;
+            }
+
+            return false;
+        }
+
+        public double IsSameArea(string originArea, string destinationArea)
+        {
+            if(originArea == destinationArea) {
+                return 0.00;
+            }
+
+            return 10.00;
+        }
+
+        public double IsSameBank(string originBank, string destinationBank)
+        {
+            if(originBank == destinationBank) {
+                return 0.00;
+            }
+
+            return 35.00;
+        }
+
+        public bool IsTotalTransferNotOverAccountBalance(double transferAmountAndFee, double accountBalance)
+        {
+            double avaliableTotal = accountBalance - transferAmountAndFee;
+            if(avaliableTotal >= 0) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- src/api/Services/TransferService.cs
+++ src/api/Services/TransferService.cs
@@ -6,78 +6,14 @@ namespace api.Services
 {
     public class TransferService
     {
-        public bool IsAccumulated(double accumulatedNumber)
-        {
-            if((accumulatedNumber >= 0) && (accumulatedNumber < 100000)) {
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool IsTransferAmount(double transferAmount)
-        {
-            if((transferAmount >= 1) && (transferAmount <= 20000)) {
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool IsTransferFee(double transferFee)
-        {
-            if(transferFee > 0) {
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool IsTotalTransferNotOverAccumulated(double transferAmountAndFee, double accumulatedNumber)
-        {
-            double transferTotal = transferAmountAndFee + accumulatedNumber;
-            if((transferTotal > 0) && (transferTotal <= 100000)) {
-                return true;
-            }
-
-            return false;
-        }
-
-        public double IsSameArea(string originArea, string destinationArea)
-        {
-            if(originArea == destinationArea) {
-                return 0.00;
-            }
-
-            return 10.00;
-        }
-
-        public double IsSameBank(string originBank, string destinationBank)
-        {
-            if(originBank == destinationBank) {
-                return 0.00;
-            }
-
-            return 35.00;
-        }
-
-        public bool IsTotalTransferNotOverAccountBalance(double transferAmountAndFee, double accountBalance)
-        {
-            double avaliableTotal = accountBalance - transferAmountAndFee;
-            if(avaliableTotal >= 0) {
-                return true;
-            }
-
-            return false;
-        }
-
+        private TransferCondition transferCondition;
         public TransferOutputModel TransferMoney(TransferInputModel transferDataInput)
         {
             bool _valid = false;
             var _code = "400";
             var _message = "error";
 
-            if(IsTransferAmount(Double.Parse(transferDataInput.transfer_amount))) {
+            if(transferCondition.IsTransferAmount(Double.Parse(transferDataInput.transfer_amount))) {
                 _valid = true;
             }
 
